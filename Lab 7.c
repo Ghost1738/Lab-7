809189879251fb54463d33161f6137c9c310e375
Alexander T Charleston <53549571+Ghost1738@users.noreply.github.com>
2024-03-07 20:19:18 -0500

    Update Lab 7.c

--- Lab 7.c
+++ Lab 7.c
@@ -1,6 +1,31 @@
+/*
+Alexander Charlston
+COP3506
+UCF Lab 7
+*/
+
+
+
 #include <stdio.h>
 
 // (Bubble sort function remains unchanged)
+
+void bubbleSort(int arr[], int n, int swaps[]) {
+    int i, j, temp;
+    for (i = 0; i < n-1; i++) {
+        for (j = 0; j < n-i-1; j++) {
+            if (arr[j] > arr[j+1]) {
+                // Swap elements
+                temp = arr[j];
+                arr[j] = arr[j+1];
+                arr[j+1] = temp;
+
+                // Count swaps for each value
+                swaps[arr[j]]++;
+            }
+        }
+    }
+}
 
 void selectionSort(int arr[], int n, int swaps[]) {
     int i, j, minIndex, temp;
@@ -19,7 +44,6 @@ void selectionSort(int arr[], int n, int swaps[]) {
 
         // Count swaps for each value
         swaps[arr[i]]++;
-        swaps[arr[minIndex]]++;
     }
 }
 
@@ -37,13 +61,13 @@ int main() {
     bubbleSort(array1, n1, swaps1);
     bubbleSort(array2, n2, swaps2);
 
-    printf("array1:\n");
+    printf("array1: Bubble\n");
     for (int i = 0; i < n1; i++) {
         printf("%d: %d swaps\n", array1[i], swaps1[array1[i]]);
     }
     printf("Total number of swaps: %d\n", swaps1[0]); // 0 index stores the total swaps
 
-    printf("\narray2:\n");
+    printf("\narray2: Bubble\n");
     for (int i = 0; i < n2; i++) {
         printf("%d: %d swaps\n", array2[i], swaps2[array2[i]]);
     }
@@ -53,13 +77,13 @@ int main() {
     selectionSort(array1, n1, swaps1);
     selectionSort(array2, n2, swaps2);
     
-    printf("array1:\n");
+    printf("array1:Selection sort\n");
     for (int i = 0; i < n1; i++) {
         printf("%d: %d swaps\n", array1[i], swaps1[array1[i]]);
     }
     printf("Total number of swaps: %d\n", swaps1[0]); // 0 index stores the total swaps
 
-    printf("\narray2:\n");
+    printf("\narray2:Selection sort\n");
     for (int i = 0; i < n2; i++) {
         printf("%d: %d swaps\n", array2[i], swaps2[array2[i]]);
     }
